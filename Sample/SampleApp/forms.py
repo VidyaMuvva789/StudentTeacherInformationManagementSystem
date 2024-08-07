8e5b9042e2eb03980c64a2cc77e7bdd29ea0126b
VidyaMuvva789 <144550928+VidyaMuvva789@users.noreply.github.com>
2024-08-07 09:40:57 +0530

    Update forms.py

--- Sample/SampleApp/forms.py
+++ Sample/SampleApp/forms.py
@@ -1,16 +1,26 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import TeacherProfile, WorkExperience
 
 class EmailForm(forms.Form):
     email = forms.EmailField(label='Email')
 
+class TeacherRegistrationForm(UserCreationForm):
+    full_name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    phone_number = forms.CharField(max_length=15)
+    highest_degree = forms.CharField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2', 'full_name', 'email', 'phone_number', 'highest_degree']
+
 class TeacherProfileForm(forms.ModelForm):
     class Meta:
         model = TeacherProfile
-        fields = ['full_name', 'dob', 'highest_degree']
-        widgets = {
-            'dob': forms.DateInput(attrs={'type': 'date'}),
-        }
+        fields = ['full_name', 'email', 'phone_number', 'highest_degree']
+
 
 class WorkExperienceForm(forms.ModelForm):
     class Meta:
